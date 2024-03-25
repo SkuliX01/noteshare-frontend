c5d4877976a624cf88ff52008cc6d430c94f8734
SkuliX01 <bartosz.krawczyk34@gmail.com>
2024-03-25 12:24:36 -0700

    Added readme section and new components from shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger,} from "@/components/ui/alert-dialog"
+import Link from 'next/link';
+import { SiBuymeacoffee } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -8,11 +10,39 @@ export default function Home() {
         <p className="text-[36px] text-center">Noteshare </p>
         <p className='text-center text-[24px]'>~ App where you can share your notes</p>
         <div className="flex justify-center align-center pt-[15px]">
-          <Button asChild className="mr-[10px]">
-            <Link href={"/Readmore"}>Read More</Link>
-          </Button>
-          <Button asChild>
-            <Link href={"/GetStarted"}>Get Started</Link>
+          <AlertDialog>
+            <AlertDialogTrigger asChild className="mr-[10px] bg-black text-white hover:bg-[#292f3d] hover:text-white">
+              <Button variant="outline">Read More</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Read More about us</AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogDescription>
+                <p className="text-black">This App let you share and read notes from other students around a world!</p>
+              </AlertDialogDescription>
+              <AlertDialogHeader>
+                <AlertDialogTitle className="text-[15px]">Frequently Asked Questions :</AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogDescription>
+              <AlertDialogTitle className="text-black">Why it's taking so long to create new notes :</AlertDialogTitle>  
+                <p className="text-black">We are using free hosting provider which are the reason for slow upload speeds</p>
+              </AlertDialogDescription>
+              <AlertDialogDescription>
+              <AlertDialogTitle className="text-black">Is it free to use? :</AlertDialogTitle>
+                  <p className="text-black">Yes app and notes are completly free to use.</p>
+              </AlertDialogDescription>
+              <AlertDialogDescription>
+                <AlertDialogTitle className="text-black">Want to support us? :</AlertDialogTitle>
+                <p className="text-black">You can support us here : <a href="https://www.paypal.com/paypalme/Bartosz341" className="text-[#0062ff]"> Paypal</a></p>
+              </AlertDialogDescription>
+              <AlertDialogFooter>
+                <AlertDialogAction className="bg-black text-white hover:bg-[#292f3d] hover:text-white">Got it</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <Button asChild variant="outline" className="mr-[10px] bg-black text-white hover:bg-[#292f3d] hover:text-white">
+            <Link href={"/getstarted"}>Get Started</Link>
           </Button>
         </div>
       </section>
