5f08622d9cc2488cd9564dd131fb48a877d50aea
SkuliX01 <bartosz.krawczyk34@gmail.com>
2024-03-30 21:40:49 +0100

    changed favicons

--- app/mycomponents/note.tsx
+++ app/mycomponents/note.tsx
@@ -1,6 +1,6 @@
 export default function Note({title, subject, description, author}){
     return (
-        <div className="w-[750px] h-auto rounded-[15px] bg-[#Ececec] shadow-lg mt-[20px] overflow-hidden ">
+        <div className="max-w-[750px] w-10/12 h-auto rounded-[15px] bg-[#Ececec] shadow-lg mt-[20px] overflow-hidden ">
             <p className="text-[24px] text-black ml-[12px] mt-[12px]">Tytuł: {title}</p>
             <p className="text-[24px] text-black ml-[12px] mt-[-3px] break-words overflow-hidden text-ellipsis max-h-[500px]">Przedmiot: {subject}</p>
             <p className="text-[15px] text-black ml-[12px] mt-[5px]">{description}</p>
